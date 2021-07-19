e40826fd13a14ae2997128a01ced0d9d1040cbbe
Frédéric-Guillon <frdric.guillon@gmail.com>
2021-07-19 21:32:32 +0200

    page-header: small correction

---
+++ tech_a_way/docs/js/script.js
@@ -0,0 +1,16 @@
+// Le module app est un objet JavaScript
+// avec des propriétés et des méthodes
+let app = {
+
+    init: function() {
+       
+        console.log('la méthode init de l\'objet app a bien été appelée');
+
+
+    },
+
+
+};
+
+
+document.addEventListener('DOMContentLoaded', app.init);
