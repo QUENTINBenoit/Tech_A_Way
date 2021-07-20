cf161bcb71179a45d553a34b79f9ab1c37132858
Frédéric-Guillon <frdric.guillon@gmail.com>
2021-07-20 20:17:58 +0200

    page-shopping-cart: matchmedia shopping cart to move div product for responsive harmony

--- tech_a_way/docs/js/resize.js
+++ tech_a_way/docs/js/resize.js
@@ -4,37 +4,50 @@
    function resize() {
        
 
-
+    /*********************start SHOPPING CART*************************/
     if (window.matchMedia("(max-width: 576px)").matches) {
 
         const picturesProduct = document.querySelectorAll('.picture_product');
         for (const pictureProduct of picturesProduct) {
-            pictureProduct.classList.add("d-none");
+            productEl = pictureProduct.closest(".product");
+            // productEl.append(pictureProduct);
+           
+            // je crée une div
+            let divPicturePrice = document.createElement("div");
+            // je lui ajoute les classes "d-flex justify-content-between picture_price_product"
+            divPicturePrice.classList.add('d-flex', 'justify-content-between', 'picture_price_product');
+            // je lui met dedans l'image
+            divPicturePrice.append(pictureProduct);
+            productEl.append(divPicturePrice);
+        }
+    }
+
+ else if (window.matchMedia("(min-width: 577px)").matches) {
+    const picturesProduct = document.querySelectorAll('.picture_product');
+    for (const pictureProduct of picturesProduct) {
+        productEl = pictureProduct.closest(".product");
+        productEl.prepend(pictureProduct);
+
+        const allDivEmpty = document.querySelectorAll('picture_price_product');
+        for (const divEmpty of allDivEmpty) {
+            remove(divEmpty);
         }
 
+    }
+}
+    /*********************end SHOPPING CART*************************/
 
 
-
-    } else if (window.matchMedia("(max-width: 767px)").matches) {
+    /*********************start PARTIAL_BRANDS*************************/
+    if (window.matchMedia("(max-width: 767px)").matches) {
         /* La largeur minimum de l'affichage est 768 px inclus */
-      
-/*********************start PARTIAL_BRANDS*************************/
         const divBrands = document.querySelectorAll('.partial_brands');
       for (const divBrand of divBrands) {
         divBrand.classList.remove("d-flex");
         divBrand.classList.add("d-none");
       }
-/*********************end PARTIAL_BRANDS*************************/
-
-
-
       } else if (window.matchMedia("(min-width: 768px)").matches) {
         /* L'affichage est inférieur à 767px de large */
-
-
-
-
-/*********************start PARTIAL_BRANDS*************************/
 
         const divBrands = document.querySelectorAll('.partial_brands');
       for (const divBrand of divBrands) {
@@ -44,9 +57,8 @@
         let carouselInitial = document.querySelector('#partial_brands_1');
             carouselInitial.classList.remove("d-none");
             carouselInitial.classList.add("d-flex");
-/*********************end PARTIAL_BRANDS*************************/
-            
         }
+/*********************end PARTIAL_BRANDS*************************/
     }
 
 
