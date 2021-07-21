765d830cbedbd7ee256df2e067a00728b33c519e
Frédéric-Guillon <frdric.guillon@gmail.com>
2021-07-21 10:46:00 +0200

    page-shopping-cart : creation JS event delete and factor events quantity shopping cart

--- tech_a_way/docs/js/shopping_cart.js
+++ tech_a_way/docs/js/shopping_cart.js
@@ -2,36 +2,36 @@ let quantity = {
 
     init: function() {
        
-    const buttonsMinus1 = document.querySelector('.quantity_product_minus_1');
-    buttonsMinus1.addEventListener('click', quantity.handleButtonMinusClick);
+    const buttonsMinus = document.querySelectorAll('.quantity_product_minus');
+    for (const buttonMinus of buttonsMinus) {
+        buttonMinus.addEventListener('click', quantity.handleButtonMinusClick);
 
-    const buttonsMinus2 = document.querySelector('.quantity_product_minus_2');
-    buttonsMinus2.addEventListener('click', quantity.handleButtonMinusClick);
+      }
 
-    const buttonsMinus3 = document.querySelector('.quantity_product_minus_3');
-    buttonsMinus3.addEventListener('click', quantity.handleButtonMinusClick);
+    const buttonsPlus = document.querySelectorAll('.quantity_product_plus');
+    for (const buttonPlus of buttonsPlus) {
+        buttonPlus.addEventListener('click', quantity.handleButtonPlusClick);
+      }
 
-    const buttonsPlus1 = document.querySelector('.quantity_product_plus_1');
-    buttonsPlus1.addEventListener('click', quantity.handleButtonPlusClick);
 
-    const buttonsPlus2 = document.querySelector('.quantity_product_plus_2');
-    buttonsPlus2.addEventListener('click', quantity.handleButtonPlusClick);
+    const buttonsDelete = document.querySelectorAll('.button_delete_product');
+    for (const buttonDelete of buttonsDelete) {
+        buttonDelete.addEventListener('click', quantity.handleButtonDelete);
+      }
 
-    const buttonsPlus3 = document.querySelector('.quantity_product_plus_3');
-    buttonsPlus3.addEventListener('click', quantity.handleButtonPlusClick);
     },
  
 
     handleButtonMinusClick: function(evt) {
 
         target = evt.currentTarget;
-        targetNameByClass = target.className
+        targetNameById = target.id
         
 
-        productNumber = targetNameByClass.charAt(targetNameByClass.length-1);
+        productNumber = targetNameById.charAt(targetNameById.length-1);
  
         // on récupère l'élément contenant la quantité actuelle
-        actualQuantityEl = document.querySelector('.actual_quantity_product_' + productNumber);
+        actualQuantityEl = document.querySelector('#actual_quantity_product_' + productNumber);
         actualQuantityEl.innerText -= 1; 
         
         if (actualQuantityEl.innerText == 0 ) {
@@ -44,19 +44,31 @@ let quantity = {
      handleButtonPlusClick: function(evt) {
 
         target = evt.currentTarget;
-        targetNameByClass = target.className
+        targetNameById = target.id
         
 
-        productNumber = targetNameByClass.charAt(targetNameByClass.length-1);
+        productNumber = targetNameById.charAt(targetNameById.length-1);
  
         // on récupère l'élément contenant la quantité actuelle
-        actualQuantityEl = document.querySelector('.actual_quantity_product_' + productNumber);
+        actualQuantityEl = document.querySelector('#actual_quantity_product_' + productNumber);
         if (actualQuantityEl.innerText != 9 ) {
         actualQuantityEl.innerText -= -1; 
-        console.log("coucou");
         
    
         }
+     
+     },
+
+     handleButtonDelete: function(evt) {
+
+        target = evt.currentTarget;
+        targetNameById = target.id
+        
+        productNumber = targetNameById.charAt(targetNameById.length-1);
+ 
+            productEl = target.closest(".product");
+            productEl.remove();
+
      
      },
 
