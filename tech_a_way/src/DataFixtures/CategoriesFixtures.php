a7f00a302a20e1b40351f12040795636812d3d3d
Etudiant O'clock <jamalelk60@gmail.com>
2021-07-27 21:35:59 +0200

    the relationship between the categories, but in a time-consuming way

--- tech_a_way/src/DataFixtures/CategoriesFixtures.php
+++ tech_a_way/src/DataFixtures/CategoriesFixtures.php
@@ -16,7 +16,7 @@ class CategoriesFixtures extends Fixture
             // I am testing a nested table to link the major categories to the relevant
             // subcategories
 
-            'Son et images',
+            'Son/Images',
             'Informatiques',
             'Objet connecté',
             'Téléphonie',
@@ -38,18 +38,63 @@ class CategoriesFixtures extends Fixture
      ];
         
 
-        foreach ($majorCategoryList as $currentCategory){
+        
             
-            $category = new Category();
+            $categorySonAndImage = new Category();
+            $categorySonAndImage->setName($majorCategoryList[0]);
+            $categorySonAndImage->setSubtitle('Son or not Son');
 
-            $category->setName($currentCategory);
-            $category->setSubtitle('Son or not Son');
-            $subCategory = new Category;
-            $subCategory->setName('Homecinema') ;
-            $category->addSubcategory($subCategory);
-            $manager->persist($subCategory);
-            $manager->persist($category);
-        }
+
+            
+            $subCategorySon = new Category;
+            $subCategorySon->setName($subCategoriesSonAndImageList[0]) ;
+            $subCategorySon->setCategory($subCategorySon);
+            $categorySonAndImage->addSubcategory($subCategorySon);
+
+
+            $subCategoryImage = new Category;
+            $subCategoryImage->setName($subCategoriesSonAndImageList[1]);
+            $categorySonAndImage->addSubcategory($subCategoryImage);
+
+
+         
+         
+
+
+
+            $manager->persist($subCategoryImage);
+            $manager->persist($subCategorySon);
+            $manager->persist($categorySonAndImage);
+
+
+            $underSubCategorySonHomeCinema=New Category;
+            $underSubCategorySonHomeCinema->setName($underSubCategorySon[0]);
+            $subCategorySon->addSubcategory($underSubCategorySonHomeCinema);
+
+
+            $underSubCategorySonSoundBar=New Category;
+            $underSubCategorySonSoundBar->setName($underSubCategorySon[1]);
+            $subCategorySon->addSubcategory($underSubCategorySonSoundBar);
+
+            $underSubCategorySonSpeaker=New Category;
+            $underSubCategorySonSpeaker->setName($underSubCategorySon[2]);
+            $subCategorySon->addSubcategory($underSubCategorySonSpeaker);
+
+            $underSubCategorySonHeadphone=New Category;
+            $underSubCategorySonHeadphone->setName($underSubCategorySon[3]);
+            $subCategorySon->addSubcategory($underSubCategorySonHeadphone);
+
+            
+
+
+
+
+            $manager->persist($underSubCategorySonHomeCinema);
+            $manager->persist($underSubCategorySonSoundBar);
+            $manager->persist($underSubCategorySonSpeaker);
+            $manager->persist($underSubCategorySonHeadphone);
+
+        
 
         
 
