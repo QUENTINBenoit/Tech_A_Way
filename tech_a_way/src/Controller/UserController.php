6d30085c487caf71fbf8287ca4f26bbc75558ce3
Frédéric-Guillon <frdric.guillon@gmail.com>
2021-07-28 16:04:49 +0200

    feature-customer-account : creation UserController and access reserved for users connected

---
+++ tech_a_way/src/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/my-account", name="account_user_")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
