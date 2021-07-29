2e470527ae1839df78637985befd47a5db2edbac
Frédéric-Guillon <frdric.guillon@gmail.com>
2021-07-29 15:40:46 +0200

    feature-customer-acount: creation CustomerType to customize customer form

--- tech_a_way/src/Controller/UserController.php
+++ tech_a_way/src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\CustomerType;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,10 +23,37 @@ class UserController extends AbstractController
         /**
      * @Route("/{id}", name="read", methods={"GET","POST"})
      */
-    public function read($id, UserRepository $userRepository): Response
+    public function read($id, UserRepository $userRepository, Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
+        $user= $userRepository->findWithAllDetails($id);
+
+        $form = $this->createForm(CustomerType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $plainPassword = $form->get('password')->getData();
+
+            if ($plainPassword) {
+                $hashedPassword = $passwordHasher->hashPassword(
+                    $user,
+                    $plainPassword
+                );
+
+                $user->setPassword($hashedPassword);
+            }
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('acount_user_read', ['id' => $user->getId()], 301);
+        }
+
+
+
+        
         return $this->render('user/read.html.twig', [
-            'user' => $userRepository->findWithAllDetails($id),
+            'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 }
