e77f9a01bd1b6495e45d977567824a6d8fea4aad
Frédéric-Guillon <frdric.guillon@gmail.com>
2021-07-29 02:35:21 +0200

    feature-customer-account: creation links and road in UserController

--- tech_a_way/src/Controller/UserController.php
+++ tech_a_way/src/Controller/UserController.php
@@ -2,22 +2,53 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+
 
 /**
- * @Route("/my-account", name="account_user_")
+ * @Route("/acount/user", name="acount_user_")
  */
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/", name="index")
+
+        /**
+     * @Route("/{id}", name="update", methods={"GET","POST"})
      */
-    public function index(): Response
+    public function update(Request $request, User $user, UserPasswordHasherInterface $passwordHasher): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $plainPassword = $form->get('password')->getData();
+
+            if ($plainPassword) {
+                $hashedPassword = $passwordHasher->hashPassword(
+                    $user,
+                    $plainPassword
+                );
+
+                $user->setPassword($hashedPassword);
+            }
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('acount_user_update');
+        }
+
+        return $this->render('user/read.update.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 }
