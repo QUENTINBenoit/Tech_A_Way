0bf61092720bb0c7f06ec64a7dd79703afedf549
Mickaël <mickael.geerardyn@gmail.com>
2021-07-29 13:40:31 +0200

    achievement of dynamic cart page to add and remove products inside it

--- tech_a_way/src/Controller/CartController.php
+++ tech_a_way/src/Controller/CartController.php
@@ -17,8 +17,10 @@ class CartController extends AbstractController
 {
     /**
      * @Route("/list", name="list")
+     * 
+     * @return Response
      */
-    public function index(SessionInterface $sessionInterface, ProductRepository $productRepository): Response
+    public function index(SessionInterface $sessionInterface, ProductRepository $productRepository)
     {
         // dd($sessionInterface->get('cart'));
         // Search the session with de 'cart' key if exist otherwise an empty array
@@ -35,8 +37,10 @@ class CartController extends AbstractController
             ];
         }
 
+        // declared variable for total price in the cart
         $total = 0;
 
+        // Loop to calculate the total for each product and total global
         foreach($cartWithData as $item) {
             $totalItem = $item['product']->getinclTaxesPrice() * $item['quantity'];
             $total += $totalItem;
@@ -54,23 +58,79 @@ class CartController extends AbstractController
      * 
      * @Route("/add/{id}", name="add", requirements={"id" ="\d+"})
      *
+     * @param integer $id
+     * @return Response
      */
     public function add(int $id, SessionInterface $sessionInterface)
     {   
         // Begin by storage a session array with the "cart" key if exist otherwise empty table
         $cart = $sessionInterface->get('cart', []);
 
+        // for each product, if it exist the quantity is increment to one more otherwise start to one
         if (!empty($cart[$id])) {
             $cart[$id]++;
         } else {
             $cart[$id] = 1;
-        };
+        }
+
+        // Set the array variable "$cart" which contain the data into the session
+        $sessionInterface->set('cart', $cart);
+
+        // Return at the index method (cart list)
+        return $this->redirectToRoute('cart_list');
+        
+    }
+
+
+    /**
+     * Method that allows to remove an item to the shopping cart
+     *
+     * @Route("/remove/{id}", name="remove_one_quantity", requirements={"id" ="\d+"})
+     * 
+     * @param integer $id
+     * @return Response
+     */
+    public function removeOneQuantity(int $id, SessionInterface $sessionInterface) {
+
+        $cart = $sessionInterface->get('cart', []);
+
+        if (!empty($cart[$id])) {
+            $cart[$id]--;
+
+            if($cart[$id] < 1){
+                unset($cart[$id]);
+            }
+        }
 
         $sessionInterface->set('cart', $cart);
 
         return $this->redirectToRoute('cart_list');
-        
+
     }
+
+    /**
+     * Method that allows to remove an item to the shopping cart
+     *
+     * @Route("/remove/{id}", name="remove_all_one_product", requirements={"id" ="\d+"})
+     * 
+     * @param integer $id
+     * @return Response
+     */
+    public function removeAllOneProduct(int $id, SessionInterface $sessionInterface) {
+
+        $cart = $sessionInterface->get('cart', []);
+
+        if (!empty($cart[$id])) {
+            
+            unset($cart[$id]);
+        }
+
+        $sessionInterface->set('cart', $cart);
+
+        return $this->redirectToRoute('cart_list');
+
+    }
+
 
     
 }
