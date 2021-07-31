a791bd8ed9c9865b17686ab8848029eeead2b1fa
Frédéric-Guillon <frdric.guillon@gmail.com>
2021-08-01 00:51:30 +0200

    feature backoffice : work on Catgories List

--- tech_a_way/src/Controller/Admin/CategoryController.php
+++ tech_a_way/src/Controller/Admin/CategoryController.php
@@ -23,8 +23,90 @@ class CategoryController extends AbstractController
      */
     public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('admin/category/index.html.twig', [
-            'categories' => $categoryRepository->findAll(),
+        $allCategories = $categoryRepository->findAll();
+
+        // 1) on récupère toutes les catégories qui ont au moins 1 parent
+        $subSubCategories = [];
+        foreach ($allCategories as $category) {
+            // si la catégorie a un parent
+            if ($category->getCategory()) {
+                
+                // si la catégorie n'a pas de parent
+                if (!$category->getCategory()->getCategory()){
+                    $subSubCategories[$category->getName()] = $category;
+                    
+                } else {
+                    if (!$category->getCategory()->getCategory()->getCategory()){
+                        $subSubCategories[$category->getName()] = $category;  
+                    } else {
+                        if (!$category->getCategory()->getCategory()->getCategory()->getCategory()){
+                            $subSubCategories[$category->getName()] = $category;  
+                        } else {
+                            if (!$category->getCategory()->getCategory()->getCategory()->getCategory()->getCategory()){
+                                $subSubCategories[$category->getName()] = $category;  
+                            }
+
+                        }
+                    }
+                }
+            } 
+        }
+        
+        // 2) on récupère toutes les catégories qui sont les plus en bas de la hiérarchie (on supprime donc les catégories intermédiaires)
+        $number = 0;
+        foreach( $subSubCategories as $subSubCategory) {
+            
+            if($number != 0) {
+                $numberCheck = 0;
+                foreach ($subSubCategories as $check) {
+                    if ($subSubCategory == $check->getCategory()) {
+                        $numberCheck++;
+                    }
+                    if ($numberCheck >=2) {
+                        unset($subSubCategories[array_search($check->getCategory(), $subSubCategories)]);
+                    }
+                } 
+            }
+            $number++;
+        }
+        // dd($subSubCategories);
+        
+        //3) on classe les catégories 
+        $subCategoriesClassified = [];
+        foreach( $subSubCategories as $subSubCategory){
+
+            if($subSubCategory->getCategory()) {
+                if($subSubCategory->getCategory()->getCategory()) {
+
+                    if($subSubCategory->getCategory()->getCategory()->getCategory()) {
+
+                        if($subSubCategory->getCategory()->getCategory()->getCategory()->getCategory()) {
+
+                        } else {
+                            $subCategoriesClassified[$subSubCategory->getCategory()->getCategory()->getCategory()->getId()][$subSubCategory->getId()] =  $subSubCategory;
+                        }
+                    } else {
+                        $subCategoriesClassified[$subSubCategory->getCategory()->getCategory()->getId()][$subSubCategory->getId()] =  $subSubCategory;
+                    }
+
+                } else {
+                    $subCategoriesClassified[$subSubCategory->getCategory()->getId()][$subSubCategory->getId()] =  $subSubCategory;
+                    $subCategoriesClassified[$subSubCategory->getCategory()->getId()]['parent 1'] =  $subSubCategory->getCategory()->getSubcategories();
+
+                }
+            }
+        }
+        // dd($subCategoriesClassified);
+    
+
+
+     
+   
+
+        return $this->render('admin/category/index2.html.twig', [
+            'categories' => $allCategories,
+            'subCategoriesClassified' => $subCategoriesClassified,
+            'colors' => ['primary', 'success', 'danger', 'warning', 'secondary', 'info'],
         ]);
     }
 
