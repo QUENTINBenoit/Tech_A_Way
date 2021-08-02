ebf17e062d9926b729de0e3b4d5d9dc27629a5ed
Frédéric-Guillon <frdric.guillon@gmail.com>
2021-08-02 18:56:33 +0200

    feature-backoffice: CollectionType to obtain a regitrationForms of 2 entities (Address & User)

--- tech_a_way/src/Form/RegistrationFormType.php
+++ tech_a_way/src/Form/RegistrationFormType.php
@@ -2,13 +2,18 @@
 
 namespace App\Form;
 
+use App\Entity\Address;
 use App\Entity\User;
+use Doctrine\DBAL\Types\IntegerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -61,14 +66,19 @@ class RegistrationFormType extends AbstractType
                 'widget' => 'single_text',
                 // this is actually the default format for single_text
                 'format' => 'yyyy-MM-dd',
-            ));
-        ;
+            ))
+            ->add('addresses', CollectionType::class, [
+                'label' => 'Votre adresse',
+                'entry_type' => AddressType::class,
+                'entry_options' => ['label' => false],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+           
         ]);
     }
 }
