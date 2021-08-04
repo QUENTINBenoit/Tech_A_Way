0a8b71da44fc80884d3087a60dd68b85caf77eb0
Mickaël <mickael.geerardyn@gmail.com>
2021-08-04 21:54:32 +0200

    begin the form type for the order entity and mode of payment entity

--- tech_a_way/src/Controller/OrderController.php
+++ tech_a_way/src/Controller/OrderController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Order;
+use App\Entity\User;
+use App\Form\ModeOfPaymentType;
+use App\Form\OrderType;
 use App\Repository\AddressRepository;
 use App\Repository\OrderRepository;
 use App\Repository\UserRepository;
@@ -27,7 +30,7 @@ class OrderController extends AbstractController
      * @Route("/list", name="address_list")
      * 
      */
-    public function customerOrder(SessionService $sessionService)
+    public function customerOrderAddressList(SessionService $sessionService)
     {
           
         return $this->render('order/index.html.twig',[
@@ -35,6 +38,40 @@ class OrderController extends AbstractController
             'total' => $sessionService->getTotal(),
         ]);
 
+    }
+
+
+    /**
+     * Method that allows to send the user at the payement choices page
+     *
+     * @Route("/payment", name="payment_list")
+     * 
+     * @return void
+     */
+    public function customerOrderPayementList(Request $request, SessionService $sessionService): Response
+    {
+        $form = $this->createForm(OrderType::class);
+        $formPayment = $this->createForm(ModeOfPaymentType::class);
+        $form->handleRequest($request);
+        $formPayment->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+        }
+
+        if ($formPayment->isSubmitted() && $formPayment->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('success', 'Votre commande a bien été envoyée');
+            $sessionService->emptyCart();
+
+            return $this->redirectToRoute('home', [], 301);
+        }
+
+        return $this->render('order/payment.html.twig', [
+            'form' => $form->createView(),
+            'formPayment' => $formPayment->createView(),
+        ]);
     }
 
 }
