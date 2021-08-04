929b789d9f599d94b9f857429779ec3bc2e9c10b
Mickaël <mickael.geerardyn@gmail.com>
2021-08-04 11:47:17 +0200

    commit to request help for an error message

--- tech_a_way/src/Controller/OrderController.php
+++ tech_a_way/src/Controller/OrderController.php
@@ -27,7 +27,7 @@ class OrderController extends AbstractController
     /**
      * Method to display informations at the order page
      * 
-     * @Route("list", name="list")
+     * @Route("list", name="address_list")
      *  
      * 
      */
@@ -35,33 +35,10 @@ class OrderController extends AbstractController
     {
           
         return $this->render('order/index.html.twig', [
-            'cart' => $sessionService->getCart(),
-   ]);
-
-    }
-
-
-    /**
-     * @Route("/address/update/{id}", name="update_address", methods={"PATCH","PUT"}, requirements={"id" = "\d+"})
-     */
-    public function updateAddress(int $id, AddressRepository $addressRepository, Request $request): Response
-    {
-        $address= $addressRepository->find($id);
-
-        $form = $this->createForm(AddressType::class, $address);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-            $this->addFlash('success', 'Votre adresse a bien été mise à jour');
-
-            return $this->redirectToRoute('order_list',[],301);
-        }
-
-        return $this->render('user/updateAddress.html.twig', [
-            'address' => $address,
-            'form' => $form->createView(),
+            'items' => $sessionService->getCart(),
+            'total' => $sessionService->getTotal(),
         ]);
+
     }
 
 }
